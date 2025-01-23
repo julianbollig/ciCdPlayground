841c84668b696a28e44ce21559e7cee994d7a593
julianbollig <julian.bollig@tngtech.com>
2025-01-23 13:21:58 +0100

    personalize main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
-    // In the following fiels you can either give a single string,
+    name: "Julian Bollig",
+    // In the following files you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Heard about it in the SWE but not really familiar with it."],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Github and Confluence mostly.",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["What other tools to use and in general a better understanding."],
   },
 });
